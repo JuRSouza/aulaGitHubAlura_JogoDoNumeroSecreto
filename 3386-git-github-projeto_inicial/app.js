814674aa3ce1e214090f9a42bac48f02fc698c2d
Juliana Souza <121642110+JuRSouza@users.noreply.github.com>
2025-01-26 12:44:50 -0300

    Alteração valor da variável numeroLimite para 20

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 20;// Alteração para fazer commit
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -65,7 +65,21 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
+/*Desafios
 
+Crie uma conta no GitHub
+Crie um repositório para um projeto pessoal.
+Faça a instalação do Git
+Crie um repositório localmente para o seu projeto pessoal
+Adicione alguns arquivos no seu repositório local
+Faça o commit das alterações
+Configure a identidade do autor do commit.
+Crie a branch Main
+Realize a conexão do seu repositório local com o remoto
+Envie as alterações no repositório local para o remoto
+Utilize o comando git status
+
+*/
 
 
 
